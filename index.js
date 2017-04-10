3625198c426967e689471adff3f9462d3e2f39ce
junshen <redwolf85911@gmail.com>
2017-04-10 10:38:34 +0200

    almost there

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
-var jsdom = require('jsdom')
-
+var argv = require('minimist')(process.argv.slice(2));
+var fs = require('fs')
 var request = require('request');
-var cheerio = require('cheerio');
 var cmd = require('node-cmd');
+var tmp = require('tmp');
 
-jsdom.env('http://www.alltobid.com/contents/16/80.html', 
-  ['http://code.jquery.com/jquery.js'], {
-  features: {
-    FetchExternalResources: ["script", 'img', 'link'],
-    ProcessExternalResources: ["script"]
-  },
-  resourceLoader: function (resource, cb) {
-    var pathname = resource.url.pathname;
+var getTmpExtFileName = function (ext) {
+  return  tmp.tmpNameSync() + '.' + ext;
+}
+var query = function() {
+  var tmpValiCodeName = getTmpExtFileName('png')
+  var tmpConvertValiCodeFile = getTmpExtFileName('png')
+  var tmpCaptchaSolution = tmp.tmpNameSync();
 
-    if (/\.js$/.test(pathname)) {
-      resource.url.pathname = pathname.replace("/js/", "/js/raw/");
-      return resource.defaultFetch(function (err, body) {
-        if (err) return callback(err);
-        callback(null, '"use strict";\n' + body);
-      });
-    } else {
-      return resource.defaultFetch(callback);
-    }
-  }
-  done: function (err, window) {
-    var $ = window.$;
+  // get an validation code image
+  request('http://www.alltobid.com/GPCarQuery.Web/Image/ValiCode?r=0.9177529939509903')
+    .pipe(fs.createWriteStream(tmpValiCodeName).on('close', function () {
+      cmd.get(`./fuzzythresh.sh ${tmpValiCodeName} ${tmpConvertValiCodeFile}`, function () {
+        cmd.get(`tesseract ${tmpConvertValiCodeFile} ${tmpCaptchaSolution} -psm 8`, function () {
+          fs.readFile(tmpCaptchaSolution + '.txt', 'utf8', function (err, valicode) {
+            valicode = valicode.replace(/\r?\n|\r/g, "");
 
-  }
-})
+            var valicodeNumber = parseInt(valicode)
+
+            console.log('valicodeNumber', valicodeNumber)
+            console.log('valicode length', valicode.length )
+            if(isNaN(valicodeNumber) || valicode.length !== 4) {
+              query()
+            } else {
+              var options = {
+                url: 'http://www.alltobid.com/GPCarQuery.Web/Home/Query',
+                headers: {
+                  'Referer': 'http://www.alltobid.com/gpcarquery.web/home/personal',
+                  'Content-Type' : 'application/x-www-form-urlencoded'
+                },
+                form: {
+                  idcard: ('' + argv['_'][0]).slice(-4),
+                  number: '' + argv['_'][1],
+                  type: 2,
+                  code: valicodeNumber
+                }
+              };
+              request.post(options, function (err, resp, body) {
+                var bodyParsed = JSON.parse(body)
+                console.log(valicode)
+                console.log(bodyParsed)
+                if(bodyParsed.code != 0) {
+                   query()
+                } else {
+                  console.log(bodyParsed)
+                }
+              })
+            }
+          })
+        })
+      })
+  }))
+  
+
+  // try query bid information
+}
+
+query()
