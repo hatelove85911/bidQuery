5ceef3e5af19eafef10479412b4005daba684766
junshen <redwolf85911@gmail.com>
2017-04-11 22:19:18 +0800

    working

--- index.js
+++ index.js
@@ -4,29 +4,33 @@ var request = require('request');
 var cmd = require('node-cmd');
 var tmp = require('tmp');
 
+var fullidno = argv[0]
+var idno = argv[0].slice(-4)
+var bidno = argv[1]
+
 var getTmpExtFileName = function (ext) {
   return  tmp.tmpNameSync() + '.' + ext;
 }
 
-
 var query = function(cookieString) {
-  cookieString = 'ASP.NET_SessionId=niuokkppr4v0wvkzara4dbs4'
   var tmpValiCodeName = getTmpExtFileName('png')
   var tmpConvertValiCodeFile = getTmpExtFileName('png')
   var tmpCaptchaSolution = tmp.tmpNameSync();
+  var cookie
 
   // get an validation code image
-  request(`http://www.alltobid.com/GPCarQuery.Web/Image/ValiCode?r=${Math.random()}`)
+  request(`http://www.alltobid.com/GPCarQuery.Web/Image/ValiCode?r=${Math.random()}`, function (err, resp, body) {
+    cookie = resp.headers['set-cookie'][0]
+    cookie = cookie.slice(0, cookie.indexOf(';'))
+  })
     .pipe(fs.createWriteStream(tmpValiCodeName).on('close', function () {
       cmd.get(`./fuzzythresh.sh ${tmpValiCodeName} ${tmpConvertValiCodeFile}`, function () {
         cmd.get(`tesseract ${tmpConvertValiCodeFile} ${tmpCaptchaSolution} -psm 8`, function () {
           fs.readFile(tmpCaptchaSolution + '.txt', 'utf8', function (err, valicode) {
             valicode = valicode.replace(/\r?\n|\r/g, "");
 
-            var valicodeNumber = parseInt(valicode)
+            var valicodeNumber = +valicode
 
-            console.log('valicodeNumber', valicodeNumber)
-            console.log('valicode length', valicode.length )
             if(isNaN(valicodeNumber) || valicode.length !== 4) {
               query()
             } else {
@@ -35,7 +39,7 @@ var query = function(cookieString) {
                 headers: {
                   'Referer': 'http://www.alltobid.com/gpcarquery.web/home/personal',
                   'Content-Type' : 'application/x-www-form-urlencoded',
-                  'Cookie': cookieString,
+                  'Cookie': cookie,
                   'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
                   'Origin': 'http://www.alltobid.com',
                   'Pragma': 'no-cache',
@@ -44,20 +48,19 @@ var query = function(cookieString) {
                   'If-Modified-Since': 0
                 },
                 form: {
-                  idcard: argv[0].slice(-4),
-                  number: argv[1],
+                  idcard: idno,
+                  number: bidno,
                   type: 2,
                   code: valicode
                 }
               };
               request.post(options, function (err, resp, body) {
                 var bodyParsed = JSON.parse(body)
-                console.log(valicode)
-                console.log(bodyParsed)
                 if(bodyParsed.code != 0) {
                    query()
                 } else {
-                  console.log(bodyParsed)
+                  var {ClientName, used, validdate} = bodyParsed
+                  console.log(ClientName, used, validdate, bidno, fullidno)
                 }
               })
             }
@@ -67,11 +70,4 @@ var query = function(cookieString) {
   }))
 }
 
-var getCookie = function () {
-  request('http://www.alltobid.com/contents/16/80.html', function (err, resp, body) {
-    // query(resp.headers['set-cookie'])
-    query('ASP.NET_SessionId=niuokkppr4v0wvkzara4dbs4')
-  })
-}
-
-getCookie()
+query()
